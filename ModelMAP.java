dc7026fe91d58de73bfc49946c33d2eebbf8cac1
LorenzooThaaBoy <Laurin@krempl.net>
2026-06-18 08:43:49 +0200

    wwsa

--- ModelMAP.java
+++ ModelMAP.java
@@ -2,3 +2,12 @@ public class ModelMAP
 {
     // - collison/check --> ModelPLAYER 
 }
+
+
+Prompt: Als Grundinformation, wir entwickeln ein Spiel in Java auf der LibGDX Engine. Wir benutzen das MVC Model. Es ist ein 
+stationärer Platfromer mit 3-5 PLatformen und einem Boden. Wir haben Sprits für Background, PLatformen und Spieler in .png. 
+Der Spieler kann springen, laufen, mit einem Schwert angreifen. Er hat 3 Leben und die Gegner fliegen und bei einer berührung 
+verliert er eins davon. Es gibt 5 Waves bevor der Spieler in einen shop kommt und sich ein Item kaufen kann. Die Items im shop sollen
+ebenfalls progressv besser werden.  
+Eine Wave ist eine abgeschlossenwenn alle gegner der Wave tot sind, die waves sollen progressiv schwerer werden, indem sie länger 
+werden und mehr gegner kommen. 
