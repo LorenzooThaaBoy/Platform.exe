60b1f496fc85b0111d7607a96b226b744abe67bc
LorenzooThaaBoy <Laurin@krempl.net>
2026-06-20 12:27:58 +0200

    prompt

--- ModelMAP.java
+++ ModelMAP.java
@@ -3,11 +3,12 @@ public class ModelMAP
     // - collison/check --> ModelPLAYER 
 }
 
-
-Prompt: Als Grundinformation, wir entwickeln ein Spiel in Java auf der LibGDX Engine. Wir benutzen das MVC Model. Es ist ein 
+/* Prompt: Als Grundinformation, wir entwickeln ein Spiel in Java auf der LibGDX Engine. Wir benutzen das MVC Model. Es ist ein 
 stationärer Platfromer mit 3-5 PLatformen und einem Boden. Wir haben Sprits für Background, PLatformen und Spieler in .png. 
 Der Spieler kann springen, laufen, mit einem Schwert angreifen. Er hat 3 Leben und die Gegner fliegen und bei einer berührung 
 verliert er eins davon. Es gibt 5 Waves bevor der Spieler in einen shop kommt und sich ein Item kaufen kann. Die Items im shop sollen
 ebenfalls progressv besser werden.  
 Eine Wave ist eine abgeschlossenwenn alle gegner der Wave tot sind, die waves sollen progressiv schwerer werden, indem sie länger 
-werden und mehr gegner kommen. 
+werden und mehr gegner kommen. Die Gegner haben den PLayer als Target, fliegen und spawnen an festen Spawnpoits am rand des Bildschirms. 
+Wenn er Spieler stirbt, kommt ein game overscreen mit einem repeat button, das ganze wird auf tastatur gespielt, mit wasd wird bewegt und 
+mit R-Mouse angrgriffen. Bei irgendwelchen relevantren fragen stell mir diese, egal was für spezifikationen zum spiel noch benötigt sind. */
